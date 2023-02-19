6b03da6d5ecff3030cbba8b9fa1ca6d31e8c3b4e
HRS <pascal@heavyrubberslave.com>
2023-02-19 17:49:41 +0100

    Switch to generic device support

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-
 import type Device from "../model/Device.js";
 import DeviceIcon from "./icons/DeviceIcon.vue";
-import {computed, reactive} from "vue";
+import { computed, reactive } from "vue";
 
 interface Props {
-  device: Device
+  device: Device;
 }
 
 const props = defineProps<Props>();
 const device = reactive<Device>(props.device);
 
 const lastRefreshed = computed<string>((): string => {
-  return device.lastRefresh ? new Date(device.lastRefresh).toISOString() :  'n/a';
+  return device.lastRefresh
+    ? new Date(device.lastRefresh).toISOString()
+    : "n/a";
+});
+
+const deviceTypeModel = computed<string>((): string => {
+  return `${device.type} ${(device.type === 'generic') ? ` (model: ${device.deviceModel})` : ''}`;
 });
 
 function formatFwVersion(fwVersion: string): string {
@@ -20,48 +25,46 @@ function formatFwVersion(fwVersion: string): string {
   const minor = Number(fwVersion.slice(-4, -2));
   const major = Number(fwVersion.slice(0, -4));
 
-  return `${major}.${minor}.${bugfix}`
+  return `${major}.${minor}.${bugfix}`;
 }
 </script>
 
 <template>
   <v-card class="rounded-sm">
     <v-card-title>
-      <DeviceIcon :componentName="device.type" class="icon" />
+      <DeviceIcon :device="device" class="icon" />
       {{ device.deviceName }}
     </v-card-title>
     <v-divider></v-divider>
     <v-card-text class="pa-0">
       <v-list>
         <v-list-item>
-          <v-list-item-header>
-            <v-list-item-title>ID</v-list-item-title>
-            <v-list-item-subtitle v-text="device.deviceId"></v-list-item-subtitle>
-          </v-list-item-header>
+          <v-list-item-title>ID</v-list-item-title>
+          <v-list-item-subtitle
+            v-text="device.deviceId"
+          ></v-list-item-subtitle>
         </v-list-item>
         <v-list-item>
-          <v-list-item-header>
-            <v-list-item-title>Type</v-list-item-title>
-            <v-list-item-subtitle v-text="device.type"></v-list-item-subtitle>
-          </v-list-item-header>
+          <v-list-item-title>Type</v-list-item-title>
+          <v-list-item-subtitle
+            v-text="deviceTypeModel"
+          ></v-list-item-subtitle>
         </v-list-item>
         <v-list-item>
-          <v-list-item-header>
-            <v-list-item-title>Firmware</v-list-item-title>
-            <v-list-item-subtitle v-text="formatFwVersion(device.fwVersion)"></v-list-item-subtitle>
-          </v-list-item-header>
+          <v-list-item-title>Firmware</v-list-item-title>
+          <v-list-item-subtitle
+            v-text="formatFwVersion(device.fwVersion)"
+          ></v-list-item-subtitle>
         </v-list-item>
         <v-list-item>
-          <v-list-item-header>
-            <v-list-item-title>Connected since</v-list-item-title>
-            <v-list-item-subtitle v-text="new Date(device.connectedSince).toISOString()"></v-list-item-subtitle>
-          </v-list-item-header>
+          <v-list-item-title>Connected since</v-list-item-title>
+          <v-list-item-subtitle
+            v-text="new Date(device.connectedSince).toISOString()"
+          ></v-list-item-subtitle>
         </v-list-item>
         <v-list-item>
-          <v-list-item-header>
-            <v-list-item-title>Last refresh</v-list-item-title>
-            <v-list-item-subtitle v-text="lastRefreshed"></v-list-item-subtitle>
-          </v-list-item-header>
+          <v-list-item-title>Last refresh</v-list-item-title>
+          <v-list-item-subtitle v-text="lastRefreshed"></v-list-item-subtitle>
         </v-list-item>
       </v-list>
     </v-card-text>
